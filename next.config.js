a86a37d8e44732df7d81e1be4054edf2e24cc7d6
miloszmos <milosz.moska@gmail.com>
2022-08-11 22:11:21 +0200

    seo: added next/head with meta tags and lang for locales

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+  compiler: {
+    styledComponents: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,29 @@ import { ApolloProvider } from '@apollo/client';
 import GlobalStyle from '../styles/globalStyles';
 import { client } from '../lib/apolloClient';
 import Layout from '../components/templates/Layout';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <ApolloProvider client={client}>
+        <Head>
+          <title>Github | Search</title>
+          <meta
+            name="description"
+            content="A place where you search for users and repositories"
+          />
+          <meta
+            property="og:title"
+            content="Search | Find what you are looking for"
+          />
+          <meta
+            property="og:description"
+            content="A place where you search for users and repositories"
+          />
+          <meta property="og:url" content="https:url" />
+          <meta property="og:type" content="https://website.com/" />
+        </Head>
         <GlobalStyle />
         <Layout>
           <Component {...pageProps} />
